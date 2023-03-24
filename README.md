52affddf04149ed5e358da954cd3a5eb41eb9435
DydikN <PingvinPain@gmail.com>
2023-03-24 23:07:38 +0200

    upd

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Node
+
+https://ibb.co/c3yN38W
+https://ibb.co/mHZX3YJ
+https://ibb.co/4dXvYjg
+https://ibb.co/WtHBGVP
--- db/contacts.js
+++ db/contacts.js
@@ -22,7 +22,7 @@ async function removeContact(contactId) {
     return null;
   }
   const [result] = contacts.splice(index, 1);
-  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return result;
 }
 
@@ -35,7 +35,7 @@ async function addContact(name, email, phone) {
   };
 
   contacts.push(newContact);
-  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
 
